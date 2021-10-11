648eff4cdc057ca23f816b2018bf5572fcfcd9b1
Sergio Garcia <sergio.gl86@gmail.com>
2021-10-11 12:49:21 +0200

    Se sigue avanzando, si hay cosas nueva, cuales?¿

---
+++ luchador.js
@@ -0,0 +1,18 @@
+import Personajes from "./personajes";
+
+class Luchador {
+  armaQueUsa;
+  destreza;
+
+  constructor(armaQueUsa, destreza) {
+    super(nombre, familia, edad, estado);
+    this.armaQueUsa = armaQueUsa;
+    this.destreza = destreza;
+  }
+
+  comunica() {
+    console.log("Primero pego y luego pregunto");
+  }
+}
+
+export default Luchador;
---
+++ rey.js
@@ -0,0 +1,15 @@
+import Personajes from "./personajes";
+
+class Rey {
+  añosDeDeinado;
+  constructor(añosDeDeinado) {
+    super(nombre, familia, edad, estado);
+    this.añosDeDeinado = añosDeDeinado;
+  }
+
+  comunica() {
+    console.log("Vais a morir todos");
+  }
+}
+
+export default Rey;
